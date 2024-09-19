35984d09fdd2d3811dd46f44b0acfe3a499cd28f
LogenDickey <123709262+LogenDickey@users.noreply.github.com>
2024-09-19 11:33:21 -0400

    Update script.js more images

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Array of image URLs
+// Array of image URLs// Updated array of image URLs
 const images = [
     'https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/17/Grass_Block_%28graphics_fast%29_JE3.png/revision/latest?cb=20200831093828',
     'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f6/Ender_Pearl_JE3_BE2.png/revision/latest/thumbnail/width/360/height/360?cb=20200512195721',
     'https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ab/Diamond_JE3_BE3.png/revision/latest/thumbnail/width/360/height/360?cb=20230924193138',
     'https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/5e/Cow_JE5_BE2.png/revision/latest?cb=20240729214026',
     'https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/28/Enderman.png/revision/latest?cb=20240729214332',
-    'https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/88/Hanging_Lantern_JE1_BE1.png/revision/latest?cb=20200207163022'
+    'https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/88/Hanging_Lantern_JE1_BE1.png/revision/latest?cb=20200207163022',
+    'https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2c/Chicken_JE2_BE2.png/revision/latest/scale-to-width/360?cb=20240729214013',
+    'https://static.wikia.nocookie.net/minecraft/images/e/e1/Warden_glowing_soul_heart.gif/revision/latest?cb=20201209225712',
+    'https://static.wikia.nocookie.net/minecraft_gamepedia/images/0/0f/Netherite_Sword_JE2_BE2.png/revision/latest/scale-to-width/360?cb=20200304213920',
+    'https://static.wikia.nocookie.net/minecraft_gamepedia/images/1/13/Enchanted_Diamond_Pickaxe.gif/revision/latest?cb=20201118111642',
+    'https://static.wikia.nocookie.net/minecraft_gamepedia/images/6/68/Bedrock_JE2_BE2.png/revision/latest?cb=20200224220504'
 ];
 
 // Copy of images array to track unused images
